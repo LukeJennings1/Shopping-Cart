d9b21cc64f08402eb39e5f88fc9426dcde26ea17
Luke Jennings <lukejennings123@gmail.com>
2022-09-30 15:41:59 +0100

    worked on styling the slider elements

--- shopping-cart/src/middle.js
+++ shopping-cart/src/middle.js
@@ -2,15 +2,28 @@ import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import Header from './header'
-import iamge from './assets/horse.jpg'
+import firstSliderBanner from './assets/banner3.jpg'
+import secondSliderBanner from './assets/cotton.jpg'
+import thirdSliderBanner from './assets/home range.jpg'
+
 
 function Middle() {
+
+  const test = () => {
+    console.log('hello')
+  }
     return (
 <div className='sliderWrapper'>
-  <AwesomeSlider animation="cubeAnimation">
-    <div data-src={iamge} />
-    <div data-src={iamge} />
-    <div data-src={iamge} />
+  
+
+  <AwesomeSlider  bullets = {false} animation="cubeAnimation">
+    <div className='firstBannerWrapper' data-src={firstSliderBanner}>
+      <button>View the full range</button>
+    </div>
+    <div className='secondBannerWrapper' data-src={secondSliderBanner}>
+      <button onClick={test}>Read All About It!</button>
+    </div>
+    <div data-src={thirdSliderBanner} />
   </AwesomeSlider>
   </div>
     )
