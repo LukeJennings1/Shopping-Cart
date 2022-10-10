db80ca109995d0cb92ffb00a03852fc217009dd1
Luke Jennings <lukejennings123@gmail.com>
2022-10-10 18:09:06 +0100

    added initial syntax to model

--- shopping-cart/src/header.js
+++ shopping-cart/src/header.js
@@ -1,24 +1,43 @@
-import logo from './assets/icons8-compass-east-100.png'
-import basketIcon from './assets/basket.png'
-import storePage from './store'
+import logo from './assets/icons8-compass-east-100.png';
+import basketIcon from './assets/basket.png';
+import storePage from './store';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 
 function Header() {
+
+const [mouseEnter, setMouseEnter] = useState(false)
+
+    function Enter(e){
+        console.log(e.target)
+        setMouseEnter(true)
+        e.target.style.color = 'blue';
+    }
+    function Exit(e){
+        console.log(mouseEnter)
+        setMouseEnter(false)
+        e.target.style.color = 'red';
+
+    }
+
+
+
     return (
         <div className="HeaderWrapper">
             <div id='left-side-wrapper'>
                 <div id="websiteTitle">Point East Clothing</div>
                 </div>
-                
+
         <div className='middleHeaderWrapper'>
                 <Link to = {'/'}>
-               <img id="logo" src={logo}></img>
+               <img id="logo"  src={logo}></img>
                 </Link>
                 </div>
 
             <div className="linkWrapper">
                 <Link to={'/store'}>
-                <button>Store</button> 
+                <button id = 'store-button' onMouseEnter={(e) => {Enter(e)}} onMouseLeave ={(e) => {Exit(e)}}>Store</button> 
                 </Link>
                 <Link to={'/Basket'}>
                 <input type='image' className='shoppingBasketIcon' src={basketIcon}></input> 
--- shopping-cart/src/middle.js
+++ shopping-cart/src/middle.js
@@ -1,7 +1,7 @@
 import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
-
+import { Link } from 'react-router-dom';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import Header from './header'
 import firstSliderBanner from './assets/banner3.jpg'
@@ -21,22 +21,24 @@ function Middle() {
     console.log('hello')
   }
     return (
-<div className='sliderWrapper'>
-
-  <AutoplaySlider  play={true}
-    cancelOnInteraction={false} // should stop playing on user interaction
+    <div className='sliderWrapper'>
+  <AutoplaySlider  
+    play={true}
+    cancelOnInteraction={true} // should stop playing on user interaction
     interval={5000} 
     bullets = {false} 
     animation="cubeAnimation">
-    <div className='firstBannerWrapper' data-src={firstSliderBanner}>
-      <button>View the full range</button>
-    </div>
-    <div className='secondBannerWrapper' data-src={secondSliderBanner}>
+        <div className='firstBannerWrapper' data-src={firstSliderBanner}>
+    <Link to={'/store'}>
+    <button >View the full range</button>
+    </Link>
+        </div>
+        <div className='secondBannerWrapper' data-src={secondSliderBanner}>
       <button onClick={test}>Read All About It!</button>
-    </div>
+        </div>
     <div data-src={thirdSliderBanner} />
   </AutoplaySlider>
-  </div>
+      </div>
     )
 }
 
